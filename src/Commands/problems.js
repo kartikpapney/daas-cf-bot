be8ce7020d1e532725a1846203c826338977a841
kartik <pdt.kartik@gmail.com>
2022-07-30 15:48:01 +0530

    Random Problems Feature implemented

---
+++ src/Commands/problems.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const {EmbedBuilder} = require('discord.js');
+const NodeCache = require( "node-cache" );
+const cache = new NodeCache();
+
+
+const getRandomProblem = async(rate) => {
+    var check = true;
+    if(!cache.get("problems")) {
+        const resp = await axios.get(`https://codeforces.com/api/problemset.problems`);
+        const problems = await resp.data.result.problems;
+        const map = {};
+        for(var p of problems) {
+            const prate = p.rating;
+            if(map[prate] === undefined) map[prate] = [];
+            map[prate].push(`https://codeforces.com/problemset/problem/${p.contestId}/${p.index}`);
+        }
+        if(cache.set("problems", map, process.env.CACHE_TIME)) {
+            console.log("cached");
+        } else {
+            console.log("can't cache");
+            return new Promise((resolve, reject) => {
+                resolve("error");
+            })
+        }
+    }
+    const map = cache.get("problems");
+    if(map[rate] === undefined) {
+        return new Promise((resolve, reject) => {
+            resolve("```" + "Can't find any probem give rating between 800-3500 " +  "```" )
+        })   
+    }
+    const problemwithrate = map[rate];
+    const problem = problemwithrate[Math.floor(Math.random() * problemwithrate.length)];
+    const embed = new EmbedBuilder()
+                .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                .setDescription(problem);
+    return new Promise((resolve, reject) => {
+        resolve({ embeds: [embed] })
+    })
+}
+
+module.exports = {getRandomProblem};
