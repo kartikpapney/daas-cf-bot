404240714bdcecc6a0bd5c65cfa953a96d461f87
kartik <pdt.kartik@gmail.com>
2022-08-01 14:18:51 +0530

    get handle now provides rating changes upto 5 contest

--- src/Commands/getUserHandle.js
+++ src/Commands/getUserHandle.js
@@ -20,10 +20,20 @@ const getUserHandle = async(handle) => {
     return new Promise(async(resolve, reject) => {
         try {
             const resp = await axios.get(`https://codeforces.com/api/user.info?handles=${handle}`);
+            const rresp = await axios.get(`https://codeforces.com/api/user.rating?handle=${handle}`)
             const user = await resp.data.result[0];
+            const urating = await rresp.data.result;
+            const rankspace = 38-(user.rank||"").length
+            var res = "";
+            for(var count=0, i=urating.length-1; i>=0 && count < 5; count++, i--) {
+                const contest = urating[i];
+                const id = "Contest " + contest.contestId;
+                const len = 35 - id.length;
+                res += `\n${id}  ${"\xa0".repeat(len)} ${contest.newRating - contest.oldRating}`;
+            }
             const embed = new EmbedBuilder()
-                        .setTitle("```" + `${handle}` + "```")
-                        .setDescription("```" + `${user.rank||"NA"}            ${user.rating||"NA"}`+ "```")
+                        .setTitle("```" + `${handle}\n${user.rank||"NA"} ${"\xa0".repeat(rankspace)} ${user.rating||"NA"}` + "```")
+                        .setDescription("```" + res + "```")
                         .setColor(color[user.rank])
             resolve({ embeds: [embed] })
         } catch(e) {
