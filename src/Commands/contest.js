67f95db237d8041dab05b7f3f592167359981d4f
kartik <pdt.kartik@gmail.com>
2022-08-12 14:05:29 +0530

    try catch update

--- src/Commands/contest.js
+++ src/Commands/contest.js
@@ -5,82 +5,96 @@ const {getCodeforcesHandle, getAuthorizedUsers} = require("./authorization.js")
 const cache = new NodeCache();
 
 const getUpcomingContests = async() => {
-    const embed = new EmbedBuilder()
-                .setTitle('Upcoming Contests @Codeforces')
-                .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
-                .setDescription(`Hi 👋\n Welcome to DaaS codeforces Bot!!!\n Below is a list of all upcoming codeforces contests.`)
-                .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
-                .setFooter({ text: 'DaaS Codeforces Bot' });
-    if(!cache.get("contests")) {
-        const resp = await axios.get('https://codeforces.com/api/contest.list');
-        const allContest = await resp.data.result;
-        const upcomingContest = [];
-        for(var idx=0; idx<allContest.length &&allContest[idx].phase==='BEFORE'; idx++) {
-            const contest = allContest[idx];
-            const datentime = new Date(contest.startTimeSeconds*1000).toLocaleString();
-            upcomingContest.push(
-                {name: contest.name + "\n" + datentime, value: `https://codeforces.com/contestRegistration/${contest.id}`}
-            )
+    try {
+        const embed = new EmbedBuilder()
+                    .setTitle('Upcoming Contests @Codeforces')
+                    .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                    .setDescription(`Hi 👋\n Welcome to DaaS codeforces Bot!!!\n Below is a list of all upcoming codeforces contests.`)
+                    .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                    .setFooter({ text: 'DaaS Codeforces Bot' });
+        if(!cache.get("contests")) {
+            const resp = await axios.get('https://codeforces.com/api/contest.list');
+            const allContest = await resp.data.result;
+            const upcomingContest = [];
+            for(var idx=0; idx<allContest.length &&allContest[idx].phase==='BEFORE'; idx++) {
+                const contest = allContest[idx];
+                const datentime = new Date(contest.startTimeSeconds*1000).toLocaleString();
+                upcomingContest.push(
+                    {name: contest.name + "\n" + datentime, value: `https://codeforces.com/contestRegistration/${contest.id}`}
+                )
+            }
+            if(cache.set("contests", upcomingContest, process.env.CACHE_TIME)) {
+                console.log("cached");
+            } else {
+                console.log("can't cache");
+                return new Promise((resolve, reject) => {
+                    resolve("error");
+                })
+            }
         }
-        if(cache.set("contests", upcomingContest, process.env.CACHE_TIME)) {
-            console.log("cached");
-        } else {
-            console.log("can't cache");
-            return new Promise((resolve, reject) => {
-                resolve("error");
-            })
-        }
+        const upcomingContest = cache.get("contests");
+        return new Promise((resolve, reject) => {
+            for(var i=upcomingContest.length-1; i>=0; i--) embed.addFields(upcomingContest[i]);
+            resolve({ embeds: [embed] })
+        })
+    } catch(e) {
+        console.log(e);
+        return new Promise((resolve, reject) => {
+            resolve("```" + "Internal server Error!!!" + "```")
+        })
     }
-    const upcomingContest = cache.get("contests");
-    return new Promise((resolve, reject) => {
-        for(var i=upcomingContest.length-1; i>=0; i--) embed.addFields(upcomingContest[i]);
-        resolve({ embeds: [embed] })
-    })
 }
 
 
 const getContestStanding = async(contest) => {
-    var response = ""
-    const embed = new EmbedBuilder()
-                .setTitle(`Contest ${contest} @Codeforces`)
-                .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
-                .setDescription(`DaaS congratulate below users to be in top 5000 in contest ${contest}`)
-                .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
-                .setFooter({ text: 'DaaS Codeforces Bot' });
-    if(!cache.get(`contest${contest}`)) {
-        try {
-            const resp = await axios.get(`https://codeforces.com/api/contest.ratingChanges?contestId=${contest}&from=1&count=5000`);
-            const result = await resp.data.result;
-            if(!result) response = "```" + "No Data Found" + "```";
-            else {
-                const users = await getAuthorizedUsers();
-                const standing = result
-                        .filter((u) => users.includes(u.handle))
-                        .map((u) => {
-                            return {"handle": u.handle, "rank": u.rank}
-                        });
-                if(cache.set(`contest${contest}`, standing, process.env.CACHE_TIME)) {
-                    console.log("cached");
-                } else {
-                    console.log("can't cache");
-                    return new Promise((resolve, reject) => {
-                        resolve("error");
-                    })
+    try {
+        var response = ""
+        const embed = new EmbedBuilder()
+                    .setTitle(`Contest ${contest} @Codeforces`)
+                    .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                    .setDescription(`DaaS congratulate below users to be in top 5000 in contest ${contest}`)
+                    .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                    .setFooter({ text: 'DaaS Codeforces Bot' });
+        if(!cache.get(`contest${contest}`)) {
+            try {
+                const resp = await axios.get(`https://codeforces.com/api/contest.ratingChanges?contestId=${contest}&from=1&count=5000`);
+                const result = await resp.data.result;
+                if(!result) response = "```" + "No Data Found" + "```";
+                else {
+                    const users = await getAuthorizedUsers();
+                    const standing = result
+                            .filter((u) => users.includes(u.handle))
+                            .map((u) => {
+                                return {"handle": u.handle, "rank": u.rank}
+                            });
+                    if(cache.set(`contest${contest}`, standing, process.env.CACHE_TIME)) {
+                        console.log("cached");
+                    } else {
+                        console.log("can't cache");
+                        return new Promise((resolve, reject) => {
+                            resolve("error");
+                        })
+                    }
                 }
+            } catch(e) {
+                return new Promise((resolve, reject) => {
+                    resolve("Invalid Contest");
+                })
             }
-        } catch(e) {
-            return new Promise((resolve, reject) => {
-                resolve("Invalid Contest");
-            })
         }
+        const standing = cache.get(`contest${contest}`)
+        standing.forEach((user) => embed.addFields(
+            {"name": user.handle, "value": user.rank+""})
+        )
+        response = { embeds: [embed] } 
+        return new Promise((resolve, reject) => {
+            resolve(response)
+        })
+    } catch(e) {
+        console.log(e);
+        return new Promise((resolve, reject) => {
+            resolve("```" + "Internal server Error!!!" + "```")
+        })
     }
-    const standing = cache.get(`contest${contest}`)
-    standing.forEach((user) => embed.addFields(
-        {"name": user.handle, "value": user.rank+""})
-    )
-    response = { embeds: [embed] } 
-    return new Promise((resolve, reject) => {
-        resolve(response)
-    })
 }
 module.exports = {getUpcomingContests, getContestStanding};
--- src/Commands/problems.js
+++ src/Commands/problems.js
@@ -5,40 +5,47 @@ const cache = new NodeCache();
 
 
 const getRandomProblem = async(rate) => {
-    if(!cache.get("problems")) {
-        const resp = await axios.get(`https://codeforces.com/api/problemset.problems`);
-        const problems = await resp.data.result.problems;
-        const map = {};
-        for(var p of problems) {
-            const prate = p.rating;
-            if(map[prate] === undefined) map[prate] = [];
-            map[prate].push(`https://codeforces.com/problemset/problem/${p.contestId}/${p.index}`);
+    try {
+        if(!cache.get("problems")) {
+            const resp = await axios.get(`https://codeforces.com/api/problemset.problems`);
+            const problems = await resp.data.result.problems;
+            const map = {};
+            for(var p of problems) {
+                const prate = p.rating;
+                if(map[prate] === undefined) map[prate] = [];
+                map[prate].push(`https://codeforces.com/problemset/problem/${p.contestId}/${p.index}`);
+            }
+            if(cache.set("problems", map, process.env.CACHE_TIME)) {
+                console.log("cached");
+            } else {
+                console.log("can't cache");
+                return new Promise((resolve, reject) => {
+                    resolve("error");
+                })
+            }
         }
-        if(cache.set("problems", map, process.env.CACHE_TIME)) {
-            console.log("cached");
-        } else {
-            console.log("can't cache");
+        const map = cache.get("problems");
+        if(map[rate] === undefined) {
             return new Promise((resolve, reject) => {
-                resolve("error");
-            })
+                resolve("```" + "Can't find any probem give rating between 800-3500 " +  "```" )
+            })   
         }
-    }
-    const map = cache.get("problems");
-    if(map[rate] === undefined) {
+        const problemwithrate = map[rate];
+        const problem = problemwithrate[Math.floor(Math.random() * problemwithrate.length)];
+        const embed = new EmbedBuilder()
+                    .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                    .setDescription(problem)
+                    .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
+                    .setFooter({ text: 'DaaS Codeforces Bot' });
         return new Promise((resolve, reject) => {
-            resolve("```" + "Can't find any probem give rating between 800-3500 " +  "```" )
-        })   
+            resolve({ embeds: [embed] })
+        })
+    } catch(e) {
+        console.log(e);
+        return new Promise((resolve, reject) => {
+            resolve("```" + "Internal server Error!!!" + "```")
+        })
     }
-    const problemwithrate = map[rate];
-    const problem = problemwithrate[Math.floor(Math.random() * problemwithrate.length)];
-    const embed = new EmbedBuilder()
-                .setImage('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
-                .setDescription(problem)
-                .setThumbnail('https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Codeforces_logo.svg/2560px-Codeforces_logo.svg.png')
-                .setFooter({ text: 'DaaS Codeforces Bot' });
-    return new Promise((resolve, reject) => {
-        resolve({ embeds: [embed] })
-    })
 }
 
 module.exports = {getRandomProblem};
